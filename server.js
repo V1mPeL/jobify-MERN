3ea60478981c604b0b0f0581e37fdff4448654e1
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-10-31 19:08:42 +0200

    back-end integration

---
+++ server.js
@@ -0,0 +1,47 @@
+import 'express-async-errors'
+import * as dotenv from 'dotenv'
+dotenv.config();
+import express from 'express'
+const app = express()
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+// router
+import jobRouter from './routes/jobRouter.js'
+// middleware
+import errorHandlerMiddleware from './middleware/errorhandlerMiddleware.js';
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('Hello World');
+});
+
+app.post('/', (req, res)=>{
+    console.log(req);
+    res.json({message:"data received", data: req.body});
+})
+
+app.use('/api/v1/jobs', jobRouter)
+
+app.use('*', (req,res) => {
+    res.status(404).json({msg:'not found'});
+})
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5100
+
+try {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(port, ()=> {
+        console.log(`server running on port: ${port}`)
+    })
+} catch (error) {
+    console.log(erorr);
+    process.exit(1)
+}
