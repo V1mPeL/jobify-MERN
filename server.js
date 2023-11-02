e69d928740ed35468e26a81cfec4173c79334f9e
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-11-02 14:33:30 +0200

    user model added

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 
 // router
-import jobRouter from './routes/jobRouter.js'
+import jobRouter from './routes/jobRouter.js';
+import authRouter from './routes/authRouter.js';
+
 // middleware
 import errorHandlerMiddleware from './middleware/errorhandlerMiddleware.js';
 
@@ -22,6 +24,7 @@ app.get('/', (req, res)=>{
 });
 
 app.use('/api/v1/jobs', jobRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.use('*', (req,res) => {
     res.status(404).json({msg:'not found'});
