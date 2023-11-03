77f1931477a94d948c0050ad9efe39e4c05d969b
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-11-03 19:34:51 +0200

    jwt provided

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import User from '../models/UserModel.js'
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
     const isFirstAccout = await User.countDocuments() === 0;
@@ -16,8 +17,18 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
     const user = await User.findOne({email: req.body.email});
+
     const isValidUser = user && await comparePassword(req.body.password, user.password);
     if(!isValidUser) throw new UnauthenticatedError('invalid credentials');
 
-    res.send('login');
+    const token = createJWT({userId:user._id, role:user.role});
+
+    const oneDay = 1000 * 60 * 60 * 24
+
+    res.cookie('token', token, {
+        httpOnly:true,
+        expires:new Date(Date.now()+oneDay),
+        secure:process.env.NODE_ENV === 'production',
+    })
+    res.status(StatusCodes.OK).json({msg:'user logged in'});
 };
