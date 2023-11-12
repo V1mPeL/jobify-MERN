b66a83cd8443c8337fb8d36797a02b6b8690f7db
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-11-12 18:05:52 +0200

    admin page

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar'
-import { useDashboardContext } from '../pages/DashboardLayout'
-import { FaTimes } from 'react-icons/fa'
-import Logo from './Logo'
-import NavLinks from './NavLinks'
+import { FaTimes } from "react-icons/fa";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import Logo from "./Logo";
 
+import NavLinks from "./NavLinks";
 const SmallSidebar = () => {
-  const {toggleSidebar, showSidebar} = useDashboardContext()
+  const { showSidebar, toggleSidebar } = useDashboardContext();
 
   return (
     <Wrapper>
-        <div className={showSidebar ? 'sidebar-container show-sidebar': 'sidebar-container'}>
-          <div className="content">
-            <button type='button' className='close-btn' onClick={toggleSidebar}>
-              <FaTimes/>
-            </button>
-            <header>
-              <Logo/>
-            </header>
-            <NavLinks/>
-          </div>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
         </div>
+      </div>
     </Wrapper>
-  )
-}
-
-export default SmallSidebar
+  );
+};
+export default SmallSidebar;
