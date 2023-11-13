cb1ba669ef659580c2136f39c850dba73c632697
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-11-13 16:27:03 +0200

    user profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,21 +3,22 @@ import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 
 export const getCurrentUser = async (req, res) => {
-    const user = await User.findOne({_id:req.user.userId});
-    const userWithoutPassword = user.toJSON();
-    res.status(StatusCodes.OK).json({ user: userWithoutPassword });
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
 };
 
 export const getApplicationStats = async (req, res) => {
-    const users = await User.countDocuments();
-    const jobs = await Job.countDocuments();
-    res.status(StatusCodes.OK).json({ users, jobs });
+  const users = await User.countDocuments();
+  const jobs = await Job.countDocuments();
+  res.status(StatusCodes.OK).json({ users, jobs });
 };
 
 export const updateUser = async (req, res) => {
-    const obj = {...req.body};
-    delete obj.password;
+  console.log(req.file);
+  const obj = { ...req.body };
+  delete obj.password;
 
-    const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
-    res.status(StatusCodes.OK).json({ msg: 'update user' });
+  const updatedUser = await User.findByIdAndUpdate(req.user.userId, obj);
+  res.status(StatusCodes.OK).json({ msg: 'update user' });
 };
