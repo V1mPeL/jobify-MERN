69464900b658fa489a115c45f860580a4c40bb3f
Sprynskyi Vitaliy <121flame121@gmail.com>
2023-11-16 14:12:05 +0200

    Stats page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import {
   HomeLayout,
   Landing,
@@ -12,22 +12,23 @@ import {
   Profile,
   Admin,
   EditJob,
-} from "./pages";
+} from './pages';
 
-import { action as registerAction } from "./pages/Register";
-import { action as loginAction } from "./pages/Login";
-import { action as addJobAction } from "./pages/AddJob";
-import { action as editJobAction } from "./pages/EditJob";
-import { action as deleteJobAction } from "./pages/DeleteJob";
-import { action as profileAction } from "./pages/Profile";
-import { loader as dashboardLoader } from "./pages/DashboardLayout";
-import { loader as allJobsLoader } from "./pages/AllJobs";
-import { loader as editJobLoader } from "./pages/EditJob";
-import { loader as adminLoader } from "./pages/Admin";
+import { action as registerAction } from './pages/Register';
+import { action as loginAction } from './pages/Login';
+import { action as addJobAction } from './pages/AddJob';
+import { action as editJobAction } from './pages/EditJob';
+import { action as deleteJobAction } from './pages/DeleteJob';
+import { action as profileAction } from './pages/Profile';
+import { loader as dashboardLoader } from './pages/DashboardLayout';
+import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
+import { loader as adminLoader } from './pages/Admin';
+import { loader as statsLoader } from './pages/Stats';
 
 export const checkDefaultTheme = () => {
-  const isDarkTheme = localStorage.getItem("darkTheme") === "true";
-  document.body.classList.toggle("dark-theme", isDarkTheme);
+  const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+  document.body.classList.toggle('dark-theme', isDarkTheme);
   return isDarkTheme;
 };
 
@@ -35,7 +36,7 @@ checkDefaultTheme();
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
@@ -44,17 +45,17 @@ const router = createBrowserRouter([
         element: <Landing />,
       },
       {
-        path: "register",
+        path: 'register',
         element: <Register />,
         action: registerAction,
       },
       {
-        path: "login",
+        path: 'login',
         element: <Login />,
         action: loginAction,
       },
       {
-        path: "dashboard",
+        path: 'dashboard',
         element: <DashboardLayout />,
         loader: dashboardLoader,
         children: [
@@ -64,32 +65,33 @@ const router = createBrowserRouter([
             action: addJobAction,
           },
           {
-            path: "stats",
+            path: 'stats',
             element: <Stats />,
+            loader: statsLoader,
           },
           {
-            path: "all-jobs",
+            path: 'all-jobs',
             element: <AllJobs />,
             loader: allJobsLoader,
           },
           {
-            path: "profile",
+            path: 'profile',
             element: <Profile />,
             action: profileAction,
           },
           {
-            path: "admin",
+            path: 'admin',
             element: <Admin />,
             loader: adminLoader,
           },
           {
-            path: "edit-job/:id",
+            path: 'edit-job/:id',
             element: <EditJob />,
             loader: editJobLoader,
             action: editJobAction,
           },
           {
-            path: "delete-job/:id",
+            path: 'delete-job/:id',
             action: deleteJobAction,
           },
         ],
---
+++ client/src/components/ChartsContainer.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+import BarChartContainer from './BarChart';
+import AreaChartComponent from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? (
+        <BarChartContainer data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
+};
+
+export default ChartsContainer;
